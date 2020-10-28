c25521cdbbae51967492b779f4792dfe94897a13
changfei.yu <156019000@qq.com>
2020-10-28 21:16:17 +0800

    xml导出时，bool类型转为整型

--- Exporters/XmlExporter.cs
+++ Exporters/XmlExporter.cs
@@ -27,6 +27,7 @@ namespace excel2json
             {"int64", new TypeMappingInfo("n_", "2")},
             {"float", new TypeMappingInfo("f_", "3")},
             {"string", new TypeMappingInfo("s_", "4")},
+            {"bool", new TypeMappingInfo("n_", "1")},
         };
 
         public string context
@@ -122,7 +123,21 @@ namespace excel2json
                             finalName = mappingInfo.prefix + table.fieldInfos[j].name;
                         }
 
-                        xmlRecord.SetAttribute(finalName, table.fieldInfos[j].datas[i].ToString());
+                        if (table.fieldInfos[j].type.Equals("bool", System.StringComparison.OrdinalIgnoreCase))
+                        {
+                            if (table.fieldInfos[j].datas[i].Equals(true))
+                            {
+                                xmlRecord.SetAttribute(finalName, "1");
+                            }
+                            else
+                            {
+                                xmlRecord.SetAttribute(finalName, "0");
+                            }
+                        }
+                        else
+                        {
+                            xmlRecord.SetAttribute(finalName, table.fieldInfos[j].datas[i].ToString());
+                        }
                     }
                     xmlTable.AppendChild(xmlRecord);
                 }
